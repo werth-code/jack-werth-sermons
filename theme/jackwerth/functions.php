d1f4b4b647465c7449bffbfeb006ee64134a8df3
werth-code <matthewwerth@gmail.com>
2026-06-22 09:49:15 -0400

    Update static snapshot (2026-06-22)

--- theme/jackwerth/functions.php
+++ theme/jackwerth/functions.php
@@ -5,7 +5,7 @@
  */
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-define( 'JW_THEME_VER', '1.0.7' );
+define( 'JW_THEME_VER', '1.0.8' );
 
 /* ------------------------------------------------------------------ setup */
 add_action( 'after_setup_theme', function () {
